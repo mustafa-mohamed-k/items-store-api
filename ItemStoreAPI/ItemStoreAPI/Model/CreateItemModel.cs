c869a5cb390c1e0394d2531440aa25a80f0ecee6
Mustafa Mohamed <mustafamohamed4014@gmail.com>
2024-02-07 11:33:38 +0300

    Remaining item endpoints
    1. Add the remaining endpoints for Item.
    2. Comments and documentation.

---
+++ ItemStoreAPI/ItemStoreAPI/Model/CreateItemModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItemStoreAPI.Model
+{
+    public class CreateItemModel
+    {
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ ItemStoreAPI/ItemStoreAPI/Model/UpdateItemModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ItemStoreAPI.Model
+{
+    public class UpdateItemModel
+    {
+        /// <summary>
+        /// The new name to set for the item.
+        /// </summary>
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The ID of the item to update.
+        /// </summary>
+        [Required]
+        public int Id { get; set; } = 0;
+
+        /// <summary>
+        /// The new description to set for the item.
+        /// </summary>
+        public string Description {  get; set; } = string.Empty;
+    }
+}
